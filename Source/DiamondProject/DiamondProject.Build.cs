f87c9ebd2edfb810564c66eaa86e5a74a12be0f4
DisturbedBanana <tompouts@gmail.com>
2025-02-10 11:42:18 +0100

    Project Commit

---
+++ Source/DiamondProject/DiamondProject.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class DiamondProject : ModuleRules
+{
+	public DiamondProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ Source/DiamondProject/DiamondProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DiamondProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADiamondProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADiamondProjectGameMode();
+};
+
+
+
