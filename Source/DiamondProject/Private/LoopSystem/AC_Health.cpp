2af3a9b8ac9a511e9a179c0f4b4c8a080d7f7a3d
Chloe-Ms <mllechloe.mathias@gmail.com>
2025-02-13 09:20:22 +0100

    [ADD] Health component and shader test

---
+++ Source/DiamondProject/Private/LoopSystem/AC_Health.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DiamondProject/Public/LoopSystem/AC_Health.h"
+
+
+// Sets default values for this component's properties
+UAC_Health::UAC_Health()
+{
+}
+
+
+// Called when the game starts
+void UAC_Health::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
---
+++ Source/DiamondProject/Public/LoopSystem/AC_Health.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AC_Health.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class DIAMONDPROJECT_API UAC_Health : public UActorComponent
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditAnywhere)
+	int MaxHealth = 3;
+
+	UPROPERTY()
+	int CurrentHealth;
+	
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Sets default values for this component's properties
+	UAC_Health();
+
+	//UFUNCTION()
+};
