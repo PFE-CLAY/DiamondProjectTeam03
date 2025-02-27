1de66cac00d913ccc9573fbd0ae941785f387f68
Yanis <rojano.yanis@gmail.com>
2025-02-27 11:16:52 +0100

    [Merge] Merge Main To GP_Puzzle

---
+++ Source/DiamondProject/Private/LoopSystem/LoopSubsystem.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LoopSystem/LoopSubsystem.h"
+
+#include "Kismet/GameplayStatics.h"
+
+void ULoopSubsystem::ReloadScene()
+{
+	UGameplayStatics::OpenLevel(this, FName(*GetWorld()->GetName()), false);
+}
---
+++ Source/DiamondProject/Public/LoopSystem/AC_Health.h
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AC_Health.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class DIAMONDPROJECT_API UAC_Health : public UActorComponent
+{
+	GENERATED_BODY()
+
+private:
+	UDELEGATE(BlueprintCallable)
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedEvent, int, CurrentHealth);
+
+	UDELEGATE(BlueprintCallable)
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeathEvent);
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int MaxHealth = 3;
+
+	UPROPERTY(BlueprintReadOnly)
+	int CurrentHealth;
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnHealthChangedEvent OnHealthChangedEvent;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnDeathEvent OnDeathEvent;
+
+protected:
+    UPROPERTY(BlueprintReadOnly)
+    bool bIsDead;	
+
+public:
+	// Sets default values for this component's properties
+	UAC_Health();
+
+	UFUNCTION(BlueprintCallable)
+	void DecreaseOneHealth();
+
+	UFUNCTION(BlueprintCallable)
+	void DecreaseHealth(int Damage);
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	UFUNCTION()
+	void ChangeHealth(int NewHealth);
+};
