8851255f31af71a85817facb9ea0bff52811aea5
Chloe-Ms <mllechloe.mathias@gmail.com>
2025-03-04 22:08:50 +0100

    [GPP][ADD] Save preplan data

---
+++ Source/DiamondProject/Public/LoopSystem/PreplanData.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "PreplanData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIAMONDPROJECT_API UPreplanData : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="PreplanData")
+	FString PreplanID;
+
+	UPROPERTY(EditAnywhere,BlueprintReadOnly)
+	TArray<TObjectPtr<UPreplanData>> PreviousDatas;
+};
