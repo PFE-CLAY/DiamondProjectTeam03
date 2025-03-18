688e18e212a510d16d640513976c92d8d90de85f
Chloe-Ms <mllechloe.mathias@gmail.com>
2025-03-18 10:20:51 +0100

    [GPP][ADD] Add preplan advice

---
+++ Source/DiamondProject/Public/LoopSystem/PreplanAdvice.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PreplanAdvice.generated.h"
+
+UCLASS()
+class DIAMONDPROJECT_API APreplanAdvice : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="PreplanAdvice")
+	FString PreplanID;
+};
