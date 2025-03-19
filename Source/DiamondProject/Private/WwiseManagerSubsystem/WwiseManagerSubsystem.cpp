0e9a67f69fb308e07893850fc7a19ca48e95e7b6
Yanis <rojano.yanis@gmail.com>
2025-03-19 17:04:11 +0100

    [GP][ADD] Wwise Subsystem and update root output path in Project settings

---
+++ Source/DiamondProject/Private/WwiseManagerSubsystem/WwiseManagerSubsystem.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "../Plugins/Wwise/Source/AkAudio/Classes/AkAudioEvent.h"
+#include "WwiseManagerSubsystem/WwiseManagerSubsystem.h"
+
+void UWwiseManagerSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	UE_LOG(LogTemp, Log, TEXT("WwiseAudioManagerSubsystem Initialized"));
+}
+
+void UWwiseManagerSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+	UE_LOG(LogTemp, Log, TEXT("WwiseAudioManagerSubsystem Deinitialized"));
+}
+
+void UWwiseManagerSubsystem::PlayEvent(UAkAudioEvent* Event, AActor* TargetActor, const FOnAkPostEventCallback& PostEventCallback)
+{
+	if (Event && TargetActor)
+	{
+		UAkGameplayStatics::PostEvent(Event, TargetActor, 0, PostEventCallback);
+		UE_LOG(LogTemp, Log, TEXT("Playing Wwise Event: %s"), *Event->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Event or TargetActor is null!"));
+	}
+}
---
+++ Source/DiamondProject/Public/WwiseManagerSubsystem/WwiseManagerSubsystem.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "../Plugins/Wwise/Source/AkAudio/Classes/AkGameplayStatics.h"
+#include "WwiseManagerSubsystem.generated.h"
+/**
+ * 
+ */
+UCLASS()
+class DIAMONDPROJECT_API UWwiseManagerSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	
+	UFUNCTION(BlueprintCallable, Category = "Wwise")
+	void PlayEvent(UAkAudioEvent* Event, AActor* TargetActor, UPARAM(meta = (Bitmask, BitmaskEnum = "/Script/AkAudio.EAkCallbackType")) const FOnAkPostEventCallback& PostEventCallback);
+};
