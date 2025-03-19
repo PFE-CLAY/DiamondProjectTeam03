a5124c43d6694db2d8b2e06b71574c93928f4a1e
Petit Choupinou <lebourhisalex11@gmail.com>
2025-03-19 12:00:08 +0100

    [GPP][ADD] Main Menu + Level choice menu
    Main menu and level menu widgets + main menu scene

---
+++ Source/DiamondProject/Private/UI/UMainMenuWidget.cpp
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/UMainMenuWidget.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+
+bool UUMainMenuWidget::Initialize()
+{
+	Super::Initialize();
+	PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	return false;
+}
+
+void UUMainMenuWidget::OnPlayButtonClicked()
+{
+	
+	LevelChoiceWidget = CreateWidget<UUserWidget>(GetWorld(), LevelChoiceWidgetClass);
+	LevelChoiceWidget->AddToViewport();
+}
+
+void UUMainMenuWidget::OnQuitApplication()
+{
+	
+	UKismetSystemLibrary::QuitGame(GetWorld(), PlayerController,EQuitPreference::Quit, true );
+}
+
+void UUMainMenuWidget::OpenLevelByName(FName LevelName)
+{
+	//UGameplayStatics::OpenLevel(GetWorld(), PlayLevelName,true);
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Clicked"));
+	if(GetWorld() == nullptr)
+		return;
+	FLatentActionInfo LatentInfo;
+	UGameplayStatics::OpenLevel(GetWorld(), LevelName);
+}
+
+void UUMainMenuWidget::OnCloseWindow()
+{
+	this->RemoveFromParent();
+}
+
+
+
+
+
+
---
+++ Source/DiamondProject/Public/UI/UMainMenuWidget.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UMainMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIAMONDPROJECT_API UUMainMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UUserWidget> LevelChoiceWidgetClass;
+
+	UPROPERTY()
+	APlayerController* PlayerController;
+	
+private:
+
+	UPROPERTY()
+	UUserWidget* LevelChoiceWidget;
+
+	
+protected:
+	UFUNCTION()
+	virtual bool Initialize() override;
+	
+	UFUNCTION(BlueprintCallable)
+	void OnPlayButtonClicked();
+
+	UFUNCTION(BlueprintCallable)
+	void OnQuitApplication();
+
+	UFUNCTION(BlueprintCallable)
+    void OpenLevelByName(FName LevelName);
+	
+	UFUNCTION(BlueprintCallable)
+	void OnCloseWindow();
+	
+};
