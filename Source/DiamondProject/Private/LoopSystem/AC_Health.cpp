5f9e040a4ce6a6603361002d0af5cc156b19c360
Disturbed <tompouts@gmail.com>
2025-05-13 18:46:42 +0200

    [GPP][EDIT] Health component
    Removed old "CanDieMultipleTimes" bool
    Added "IsInvincible" bool, allowing usage of the "OnHealthChanged" event on an invincible entity, instead of exploiting the "OnDeath" one, which seemed unethical

--- Source/DiamondProject/Private/LoopSystem/AC_Health.cpp
+++ Source/DiamondProject/Private/LoopSystem/AC_Health.cpp
@@ -27,15 +27,14 @@ void UAC_Health::DecreaseHealth(int Damage)
 
 void UAC_Health::ChangeHealth(int NewHealth)
 {
-	NewHealth = FMath::Clamp(NewHealth, 0, MaxHealth);
-	CurrentHealth = NewHealth;
-
+	if (!bIsInvincible) {
+		NewHealth = FMath::Clamp(NewHealth, 0, MaxHealth);
+		CurrentHealth = NewHealth;
+	}
+	
 	if (CurrentHealth == 0 && !bIsDead) {
 		OnDeathEvent.Broadcast();
-		
-		if (!bCanDieMultipleTimes) {
-			bIsDead = true;
-		}
+		bIsDead = true;
 		
 	} else {
 		OnHealthChangedEvent.Broadcast(CurrentHealth);
