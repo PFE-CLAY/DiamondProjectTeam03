3f86c5c4f1e88fbbff5ea7dec719ca4eb55ff187
Chloe-Ms <mllechloe.mathias@gmail.com>
2025-06-14 00:05:02 +0200

    [GPP][MOD] FIx full heal

--- Source/DiamondProject/Private/LoopSystem/AC_Health.cpp
+++ Source/DiamondProject/Private/LoopSystem/AC_Health.cpp
@@ -22,22 +22,22 @@ void UAC_Health::DecreaseOneHealth()
 
 void UAC_Health::DecreaseHealth(int Damage, const AActor* DamageDealer) 
 {
-	ChangeHealth(CurrentHealth - Damage, DamageDealer);
-}
-
-void UAC_Health::ChangeHealth(int NewHealth, const AActor* DamageDealer)
-{
 	if (!DamageDealer) {
 		UE_LOG(LogTemp, Error, TEXT("NO DAMAGE DEALER PROVIDED!"));
 		return;
 	}
-	
+
 	for (const auto& IgnoredClass : DamageIgnoreClasses) {
 		if (DamageDealer->IsA(IgnoredClass)) {
 			return;
 		}
 	}
+	
+	ChangeHealth(CurrentHealth - Damage, DamageDealer);
+}
 
+void UAC_Health::ChangeHealth(int NewHealth, const AActor* DamageDealer)
+{
 	if (!bIsInvincible) {
 		NewHealth = FMath::Clamp(NewHealth, 0, MaxHealth);
 		CurrentHealth = NewHealth;
