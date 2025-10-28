f028c05de67f5d4cfb95eb78fa05ecfb5494d5b2
Yanis <rojano.yanis@gmail.com>
2025-10-28 09:48:00 +0100

    LevelStreamingSubsystem with test map

---
+++ Source/DiamondProject/Private/LevelStreamingSubSystem/LevelStreamingSubSystem.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LevelStreamingSubSystem/LevelStreamingSubSystem.h"
+
+#include "Kismet/GameplayStatics.h"
+
+ULevelStreaming* ULevelStreamingSubSystem::LoadLevelByName(FName LevelName, bool bMakeVisibleAfterLoad, bool bShouldBlockOnLoad)
+{
+	FLatentActionInfo LatentInfo;
+	UGameplayStatics::LoadStreamLevel(this, LevelName, true, true, LatentInfo);
+
+	LoadedLevelsCache.Add(LevelName, UGameplayStatics::GetStreamingLevel(this, LevelName));
+	return LoadedLevelsCache[LevelName];
+}
+
+void ULevelStreamingSubSystem::UnloadLevelByName(FName LevelName)
+{
+	FLatentActionInfo LatentInfo;
+	
+	if (!LoadedLevelsCache.Contains(LevelName)) {
+		UE_LOG(LogTemp, Warning, TEXT("Level %s is not loaded or not tracked in cache."), *LevelName.ToString());
+		return;
+	}
+	UGameplayStatics::UnloadStreamLevel(this, LevelName, LatentInfo, true);
+	LoadedLevelsCache.Remove(LevelName);
+}
---
+++ Source/DiamondProject/Public/LevelStreamingSubSystem/LevelStreamingSubSystem.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "LevelStreamingSubSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIAMONDPROJECT_API ULevelStreamingSubSystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+private:
+	
+	UPROPERTY()
+	TMap<FName, TObjectPtr<ULevelStreaming>> LoadedLevelsCache;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "LevelStreaming SubSystem", meta = (AdvancedDisplay = "1"))
+	ULevelStreaming* LoadLevelByName(FName LevelName, bool bMakeVisibleAfterLoad = true, bool bShouldBlockOnLoad = true);
+
+	UFUNCTION(BlueprintCallable, Category = "LevelStreaming SubSystem")
+	void UnloadLevelByName(FName LevelName);
+};
