9571e248aedefee6d416893a08dc2100488b4e32
Emile Birraux <112955984+Birox1er@users.noreply.github.com>
2025-12-03 11:47:02 +0100

    [GPP][FIX] not falling through when in backstage

--- Source/DiamondProject/Private/LevelStreamingSubSystem/LevelStreamingComponent.cpp
+++ Source/DiamondProject/Private/LevelStreamingSubSystem/LevelStreamingComponent.cpp
@@ -91,11 +91,11 @@ void ULevelStreamingComponent::ForceUnloadLevels(TArray<UWorld*> Levels) const
 	}
 }
 
-void ULevelStreamingComponent::EnableStreamingVolumes(bool Enable) const
+void ULevelStreamingComponent::EnableStreamingVolumes(bool Disable) const
 {
 	for (ALevelStreamingVolume* StreamingVolume : FoundStreamingVolumes) {
 		if (StreamingVolume) {
-			StreamingVolume->bDisabled = Enable;
+			StreamingVolume->bDisabled = Disable;
 			UE_LOG(LogTemp, Log, TEXT("[ULevelStreamingComponent::EnableStreamingVolumes] Streaming Volume %s collision set to %s."), *StreamingVolume->GetName(), Enable ? TEXT("Enabled") : TEXT("Disabled"));
 		}
 	}
